f6b5a664ec63d7e535d3e60b17dc57e7791abe2b
Alex Ilgayev <alex.ilgayev@gmail.com>
2021-12-14 21:21:56 +0200

    Removed the modules completely

--- malsub.py
+++ malsub.py
@@ -35,16 +35,16 @@ Input formats (hash values or files are given as default depending on options):
 Supported hash values: MD5, SHA1, SHA-256 and SHA-512.
 
 Examples:
-  - Retrieve user quota for AVCaesar and Hybrid Analysis and be verbose:
-$ python3 malsub.py -a avc,ha -q -v
+  - Retrieve user quota for Hybrid Analysis and be verbose:
+$ python3 malsub.py -a ha -q -v
 
   - Submit an URL for analysis to VirusTotal and output verbose and debug
     messages:
 $ python3 malsub.py -vva VirusTotal -su <url>
 
-  - Submit two files to maltracker, QuickSand and VirusTotal and pause 60
+  - Submit two files to maltracker and VirusTotal and pause 60
     seconds between submissions:
-$ python3 malsub.py -a mt,qs,virustotal -p 60 -s <file1> <file2>
+$ python3 malsub.py -a mt,virustotal -p 60 -s <file1> <file2>
 
   - Retrieve reports for a file, for files under a recursive path and for a hash
     value:
@@ -72,8 +72,8 @@ from docopt import docopt, printable_usage
 
 from malsub.core import main
 
-if __name__ == '__main__':
-  exit(main.run(docopt(__doc__), printable_usage(__doc__)))
+if __name__ == "__main__":
+    exit(main.run(docopt(__doc__), printable_usage(__doc__)))
 
 
 # https://malwareconfig.com/api/
@@ -86,12 +86,12 @@ if __name__ == '__main__':
 # CONTRIBUTING.md
 
 # -b
-    # open URLs in the browser
+# open URLs in the browser
 
 # make groups
-    # -g mal
-    # -g intel
-    # -g url
+# -g mal
+# -g intel
+# -g url
 # relative/full path for data/ and downl/
 
 # password-protected samples
